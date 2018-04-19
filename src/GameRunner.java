500f6ac40bc8e0aa7964497bdaed40033e71c73f
oconr003 <rachel-oc@hotmail.co.uk>
2018-04-19 11:43:57 +0100

    For my own records

---
+++ src/GameRunner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GameRunner {
+
+  public static void main (String[] args)
+  {
+    Functions currentGame = new Functions();
+    
+    currentGame.startGame();
+    Scanner scanner = new Scanner(System.in);
+
+    do {
+
+      System.out.print("Enter 'i' for up, 'm' for down, 'j' for left, 'l' for right: ");
+	  currentGame.showBoard();
+    	  currentGame.compass();
+      String userInput = scanner.next();
+      currentGame.movePlayer(userInput);
+      
+      
+    } while (!currentGame.isGameOver());
+  }
+
+}
